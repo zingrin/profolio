4c3d972fb64e534064bb996d6cc754deaeb86d6e
zingrin <jerinloncheu@gmail.com>
2025-08-11 15:51:17 +0600

    skills tools add

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import SkillsSlider from "../pages/SkillsSlder";
 
+
 const Skills = () => {
   return (
-    <div className="max-w-4xl mx-auto p-6 bg-gray-800 rounded-xl shadow-lg overflow-hidden">
+    <section className="max-w-4xl mx-auto p-6 bg-gray-800 rounded-xl shadow-lg overflow-hidden">
+      <h2 className="text-3xl font-extrabold text-cyan-400 mb-6 text-center">
+        My Technical Skills
+      </h2>
       <SkillsSlider />
-    </div>
+    </section>
   );
 };
 
